813a534220f280d2b61672189648ded364d54555
cristobalwalters12 <cristobalwalters@gmail.com>
2024-01-12 11:02:26 -0300

    Add validation for nombre_sensor field in CreateNombresSensoreDto

--- src/nombres_sensores/dto/create-nombres_sensore.dto.ts
+++ src/nombres_sensores/dto/create-nombres_sensore.dto.ts
@@ -1,1 +1,6 @@
-export class CreateNombresSensoreDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+export class CreateNombresSensoreDto {
+  @IsNotEmpty()
+  @IsString()
+  nombre_sensor: string;
+}
