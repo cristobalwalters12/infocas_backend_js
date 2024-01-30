8eba32ff68c016593a0e653a2a11cfe8af0f279a
cristobalwalters12 <cristobalwalters@gmail.com>
2024-01-30 12:39:01 -0300

    Refactor CORS configuration in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { RetryInterceptor } from './common/interceptors/retry.interceptors';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
   app.useGlobalInterceptors(new RetryInterceptor());
-  app.enableCors({
-    origin: configService.get('CORS_ORIGIN'),
-    credentials: true,
-  });
-
+  app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
