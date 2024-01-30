b8a4180fcdf6af213bada3c30f4d21a3b18c3a85
cristobalwalters12 <cristobalwalters@gmail.com>
2024-01-30 12:45:44 -0300

    Update CORS origin in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { RetryInterceptor } from './common/interceptors/retry.interceptors';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
   app.useGlobalInterceptors(new RetryInterceptor());
   app.enableCors({
-    origin: configService.get('CORS_ORIGIN'),
+    origin: 'https://infocas-js-production.up.railway.app',
     credentials: true,
   });
 
