9930f4c20f7c8ff28c3e3bc6194e911f78130ce6
cristobalwalters12 <cristobalwalters@gmail.com>
2024-11-10 22:14:19 -0300

    Actualizar cron para respaldo de sensores a las 3:30 AM de lunes a domingo

--- src/common/services/Task.service.ts
+++ src/common/services/Task.service.ts
@@ -182,7 +182,7 @@ export class TaskService {
       console.error('Error al obtener nombres de controladores:', error);
     }
   }
-  //@Cron('0 0 * * *')
+  @Cron('0 30 3 * *  1-7')
   async respaldoSensores2024() {
     const fecha_fin: Date = new Date();
     const fecha_inicio: Date = new Date();
