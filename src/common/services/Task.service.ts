bb84bd505b16efd4e79743d8d6395b8467297e69
cristobalwalters12 <cristobalwalters@gmail.com>
2025-04-07 22:04:51 -0400

    Ajustar el desfase horario en TaskService para reflejar correctamente la zona horaria

--- src/common/services/Task.service.ts
+++ src/common/services/Task.service.ts
@@ -32,10 +32,10 @@ export class TaskService {
       console.log(`Fecha ajustada a ${timeZone}: ${formattedDate}`);
       const sensores = await this.nombresSensoresService.findAll();
       const now = new Date();
-      now.setHours(now.getHours() - 3);
+      now.setHours(now.getHours() - 5);
       const hourStart: string = now.toTimeString().split(' ')[0];
       const current = new Date();
-      current.setHours(current.getHours() - 2);
+      current.setHours(current.getHours() - 4);
       const hourEnd: string = current.toTimeString().split(' ')[0];
       const sensoresSinDatos: string[] = [];
       const sensoressinDatosId: number[] = [];
