70fd3ad808524b393ab2f1bcd26e9db6676aa9da
cristobalwalters12 <cristobalwalters@gmail.com>
2025-04-07 21:37:58 -0400

    Ajustar la lógica de manejo de horas en TaskService para corregir el desfase horario

--- src/common/services/Task.service.ts
+++ src/common/services/Task.service.ts
@@ -32,10 +32,10 @@ export class TaskService {
       console.log(`Fecha ajustada a ${timeZone}: ${formattedDate}`);
       const sensores = await this.nombresSensoresService.findAll();
       const now = new Date();
-      now.setHours(now.getHours() - 4);
+      now.setHours(now.getHours() - 3);
       const hourStart: string = now.toTimeString().split(' ')[0];
       const current = new Date();
-      current.setHours(current.getHours() - 3);
+      current.setHours(current.getHours() - 2);
       const hourEnd: string = current.toTimeString().split(' ')[0];
       const sensoresSinDatos: string[] = [];
       const sensoressinDatosId: number[] = [];
